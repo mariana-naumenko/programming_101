bfe83e42bb64584f9bd3b0e356fe32920a271ac7
mariana-naumenko <mariana.naumenko@accenture.com>
2026-01-07 10:42:06 +0100

    Add Main class with main method for output

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🚀 Java is running in GitHub Codespaces!");
+        System.out.println("If you can read this, everything works 😄");
+    }
+}
